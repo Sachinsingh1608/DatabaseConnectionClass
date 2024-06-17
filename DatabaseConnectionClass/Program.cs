a4fb4d9fda0e0652d92bc2ed53ab3b38b73f9c19
Sachinsingh1608 <sk379403@gmail.com>
2024-06-17 14:28:01 +0530

    main

--- DatabaseConnectionClass/Program.cs
+++ DatabaseConnectionClass/Program.cs
@@ -26,14 +26,14 @@ namespace DatabaseConnectionClass
         }
        public bool Save()
         {
-            string lsConnStr = "Integrated Security=SSPI; Persist Security Info=False; Initial Catalog=C#Training" +
-                "Data Source = LAPTOP-LFHQRLA5\\SQLEXPRESS";
-                string lsQuery = "";
+            string lsConnStr = "Integrated Security=SSPI; Persist Security Info=False; Initial Catalog=C#Training; Data Source=LAPTOP-LFHQRLA5\\SQLEXPRESS";
+
+            string lsQuery = "";
             using(SqlConnection lobjCon = new SqlConnection(lsConnStr))
             {
                 if(emp_id == 0)
                 {
-                    lsQuery = "INSERT INTO EmployeeNew (first_name,last_name.birth_date,sex,age,address" +
+                    lsQuery = "INSERT INTO EmployeeNew (first_name,last_name,birth_date,sex,age,address" +
                         ",salary,dept_id)";
                     lsQuery += "VALUES('" + first_name + "','" + last_name + "' ,'" + ((DateTime)birth_date).ToString("dd-MMM-yyyy") + "','";
 
@@ -44,7 +44,7 @@ namespace DatabaseConnectionClass
 
                 else
                 {
-                    lsQuery = "UPDATE EmployeeNew SET first_name = '"+first_name + "',last_name='"+last_name + "',birth_date='"+((DateTime)birth_date).ToString("dd-MMM-yyyy")+ "','";
+                    lsQuery = "UPDATE EmployeeNew SET    first_name = '" + first_name + "', first_name = '"+first_name + "',last_name='"+last_name + "',birth_date='"+((DateTime)birth_date).ToString("dd-MMM-yyyy")+ "','";
                     lsQuery += "sex='" + sex + "',age=" + age.ToString() + ",address ='" + address + ",salary=" + salary.ToString() + ",dept_id='" + dept_id.ToString() + ")";
                     lsQuery += "WHERE  emp_id = " + emp_id.ToString();
                 }
@@ -65,11 +65,68 @@ namespace DatabaseConnectionClass
             }
             return true;
         }
+
+        public void ReadInput()
+        {
+            Console.WriteLine("First Name");
+            first_name = Console.ReadLine();
+
+            Console.WriteLine("Last Name");
+            last_name = Console.ReadLine();
+
+            Console.WriteLine("DOB");
+            birth_date=DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("SEX");
+            sex = Console.ReadLine();
+
+            TimeSpan lobjTimeSpan = DateTime.Now.Subtract((DateTime)birth_date);
+
+
+            age = lobjTimeSpan.TotalDays / 365.0;
+            Console.WriteLine("Address");
+            address = Console.ReadLine();
+
+            Console.WriteLine("Salary");
+            salary = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dept_id");
+            dept_id = int.Parse(Console.ReadLine());    
+        }
+        public void DisplayEmployee()
+        {
+            Console.WriteLine($"name {first_name} {last_name}");
+            Console.WriteLine($"Date of Birth: {birth_date}");
+            Console.WriteLine($"Sex: {sex}");
+            Console.WriteLine($"address: {address}");
+            Console.WriteLine($"salary: {salary}");
+            Console.WriteLine($"dept_id: {dept_id}");
+        }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Employee lobjEmp = new Employee();
+            bool lbExit = false;
+            while(!lbExit)
+            {
+                Console.WriteLine("Enter Exit 0");
+                Console.WriteLine("Enter Add Employee 1");
+                Console.WriteLine("Enter Find Employee 2");
+                Console.WriteLine("Enter Delete Employee 3");
+              string lsChoice = Console.ReadLine();
+                switch (lsChoice)
+                {
+                    case "0":
+                        lbExit = true; 
+                        break;
+                    case "1":
+                        lobjEmp.ReadInput();
+                        lobjEmp.Save();
+                        break;
+                }
+            }
         }
     }
 }
